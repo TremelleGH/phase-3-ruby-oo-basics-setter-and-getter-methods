b01f1e5b9a45e7ad7f1f8c8c37df7584bb1387fc
Tremelle Ancrum <tancrum1996@gmail.com>
2022-07-19 13:56:24 -0400

    commit tree db0059249f610b3327ad1aca08aa05b036b990ba

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 
 end
+
+
+crash = Dog.new
+crash.name = "Crash"
+crash.name
+crash.breed = "Yorkie"
+crash.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
 
 end
+
+
+crash = Person.new
+crash.name = "Crash"
+crash.name
+crash.job = "Pilot"
+crash.job
